fd1bd15082dd17aeefee0f0e58a4f85a8c1ee49b
chevalmuscle <me@marcantoinejean.com>
2025-09-23 19:37:52 -0400

    add telescope vimgrep args

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,6 +86,20 @@ require('nvim-treesitter.configs').setup {
 
 -- telescope
 require("telescope").load_extension("live_grep_args")
+require("telescope").setup({
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--ignore-case",
+        }
+    }
+})
 
 -- nvim-tree
 require("nvim-tree").setup()
@@ -119,9 +133,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Auto-format ("lint") on save.
         -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
         if not client:supports_method('textDocument/willSaveWaitUntil')
-             and client:supports_method('textDocument/formatting') then
+            and client:supports_method('textDocument/formatting') then
             vim.api.nvim_create_autocmd('BufWritePre', {
-                group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
                 buffer = args.buf,
                 callback = function()
                     vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
